b01c397e2e4ca7124615f12a0b97ff6525bceb93
Saul Tobias <saul.tobias@fdmgroup.com>
2020-03-28 17:32:24 -0500

    added decision making example

---
+++ DecisionMakingApp/DecisionMakingApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DecisionMakingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string beerName;
+
+            Console.WriteLine("Please enter your age");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            if(age >= 21)
+            {
+                Console.WriteLine("You are {0} years old. You can buy alcohol\n", age);
+                Console.WriteLine("What would you like to buy?");
+                beerName = Console.ReadLine();
+                Console.WriteLine();
+
+                switch(beerName.ToUpper())
+                {
+                    case "CORONA":
+                        Console.WriteLine("{0} beer is your option. ", beerName.ToUpper());
+                        break;
+                    case "BUD LIGHT":
+                        Console.WriteLine("{0} beer is your option. ", beerName.ToUpper());
+                        break;
+                    case "MODELO":
+                        Console.WriteLine("{0} beer is your option. ", beerName.ToUpper());
+                        break;
+                    default:
+                        Console.WriteLine("{0} beer is not available. ", beerName.ToUpper());
+                        break;
+                }
+            } else
+            {
+                Console.WriteLine("You are {0} years old. You cannot buy alcohol.", age);
+            }
+        }
+    }
+}
