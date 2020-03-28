3ac6ae906a69675c78f13c720e3781e0dde33f35
Saul Tobias <saul.tobias@fdmgroup.com>
2020-03-28 16:25:16 -0500

    added tutorialspoint examples

---
+++ DataTypeApp/DataTypeApp/Program.cs
@@ -0,0 +1,42 @@
+﻿//Application shows available data types
+
+
+using System;
+
+namespace DataTypeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool boolType = false;
+            char charType = 'a';
+            double doubleType = 4.5;
+            float floatType = 4.5f;
+            int intType = 6;
+            long longType = 4575993903;
+
+            Console.WriteLine("These are some data types in C# \n");
+
+            Console.WriteLine("bool: {0}", boolType);
+            Console.WriteLine("char: {0}", charType);
+            Console.WriteLine("double: {0}", doubleType);
+            Console.WriteLine("float: {0}", floatType);
+            Console.WriteLine("int: {0}", intType);
+            Console.WriteLine("long: {0}", longType);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Showcasing method sizeof() for data types \n");
+
+            Console.WriteLine("Size of bool: {0}", sizeof(bool));
+            Console.WriteLine("Size of char: {0}", sizeof(char));
+            Console.WriteLine("Size of double: {0}", sizeof(double));
+            Console.WriteLine("Size of float: {0}", sizeof(float));
+            Console.WriteLine("Size of int: {0}", sizeof(int));
+            Console.WriteLine("Size of ling: {0}", sizeof(long));
+
+
+        }
+    }
+}
---
+++ RectangleApplication/RectangleApplication/Program.cs
@@ -0,0 +1,48 @@
+﻿//using is used to include namespaces in your application
+using System;
+
+namespace RectangleApplication
+{
+    //class declaration
+    class Rectangle
+    {
+        //member variable
+        double width;
+        double length;
+
+        public void acceptDetails() {
+            width = 4.9;
+            length = 5.4;
+        }
+
+        public double getArea()
+        {
+            return width * length;
+        }
+
+        public void display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", getArea());
+        }
+
+    }
+
+    //class with main
+    //main is entry point of program
+    //where code starts execution
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate object from class Rectangle
+            Rectangle rect = new Rectangle();
+            //call on method members of object rect
+            rect.acceptDetails();
+            rect.display();
+            //waits until enter is pressed to end program
+            Console.ReadLine();
+        }
+    }
+}
