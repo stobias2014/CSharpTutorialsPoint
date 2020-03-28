24fe02739c2e15cc4a8768b305b859123483d061
Saul Tobias <saul.tobias@fdmgroup.com>
2020-03-28 16:52:03 -0500

    added variable definition example

---
+++ VariableDefinitionApp/VariableDefinitionApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace VariableDefinitionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable definitions
+            int i;
+            double d;
+            bool b;
+            char c;
+
+            //initializing varaibles
+            i = 5;
+            d = 7.65;
+            b = false;
+            c = 'g';
+
+            Console.WriteLine("i = {0}", i);
+            Console.WriteLine("d = {0}", d);
+            Console.WriteLine("b = {0}", b);
+            Console.WriteLine("c = {0}", c);
+
+            //accept input from user
+            Console.WriteLine("Enter a number: ");
+            i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You entered: {0}", i);
+
+            
+
+        }
+    }
+}
