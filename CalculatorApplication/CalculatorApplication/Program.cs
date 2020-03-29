681893f80da304545d5d67012b3f612d8269287d
Saul Tobias <saul.tobias@fdmgroup.com>
2020-03-28 20:35:42 -0500

    added nullable example

---
+++ CalculatorApplication/CalculatorApplication/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace CalculatorApplication
+{
+    class NumberManipulator
+    {
+        public int findMax(int n1, int n2) {
+            int max;
+
+            if(n1 > n2) {
+                max = n1;
+            } else {
+                max = n2;
+            }
+
+            return max;
+        }
+
+        public int fact(int n)
+        {
+            int result;
+
+            if(n ==1)
+            {
+                return 1;
+
+            }
+            else
+            {
+                result = fact(n - 1) * n;
+                return result;
+            }
+
+
+        }
+
+    }
+
+    class Test
+    {
+        static void Main(String[] args)
+        {
+            int n1 = 105;
+            int n2 = 340;
+            int n3 = 4;
+            int max;
+
+            NumberManipulator nm = new NumberManipulator();
+            max = nm.findMax(n1, n2);
+
+            Console.WriteLine("Max from {0} and {1} is {2}", n1, n2, max);
+            Console.WriteLine("Factorial of {0} = {1}", n3, nm.fact(n3));
+        }
+        
+    }
+
+}
---
+++ NullableApp/NullableApp/Program.cs
@@ -0,0 +1,32 @@
+﻿//nullable type - you can assign regular data type as well as null value (?)
+
+using System;
+
+namespace NullableApp
+{
+    class Nullables
+    {
+        static void Main(string[] args)
+        {
+            //syntax for nullable types
+            int? n1 = null;
+            int? n2 = 4;
+
+            double? d1 = new double?();
+            double? d2 = 3.43;
+            double d3;
+
+            bool? b = new bool?();
+
+            Console.WriteLine("Nullable: {0}, {1}, {2}, {3}", n1, n2, d1, d2);
+            Console.WriteLine("Nullable bool: {0}\n", b);
+
+            //5.1 default value
+            d3 = d1 ?? 5.1;
+            Console.WriteLine("?? can be used to assign some default value.");
+            Console.WriteLine("d3 = {0}", d3);
+
+
+        }
+    }
+}
