7765fd95c0edc01fa2d9016f3f36a7abe77e30c3
Saul Tobias <saul.tobias@fdmgroup.com>
2020-04-01 14:48:36 -0500

    added regex example

---
+++ PreprocessorDirectiveApp/PreprocessorDirectiveApp/Program.cs
@@ -0,0 +1,28 @@
+﻿//preprocessor directive used to give instructions to compiler to preprocess info before comp
+//used to help conditional compilation
+
+//preprocessor declarations
+#define DEBUG
+#define VC_V10
+
+using System;
+
+namespace PreprocessorDirectiveApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //only true will compile
+#if (DEBUG && !VC_V10)
+            Console.WriteLine("DEBUG is defined");
+#elif (!DEBUG && VC_V10)
+            Console.WriteLine("VC_V10 is defined");
+#elif (DEBUG && VC_V10)
+            Console.WriteLine("DEBUG and VC_V10 are defined");
+#else
+         Console.WriteLine("DEBUG and VC_V10 are not defined");
+#endif
+        }
+    }
+}
---
+++ RegularExpressionApp/RegularExpressionApp/Program.cs
@@ -0,0 +1,28 @@
+﻿//regular expression is a pattter that could be matched against an input
+
+using System;
+using System.Text.RegularExpressions;
+
+namespace RegularExpressionApp
+{
+    class Program
+    {
+        private static void showMatch(string text, string expr)
+        {
+            Console.WriteLine("The expression: {0}", expr);
+            MatchCollection mc = Regex.Matches(text, expr);
+
+            foreach (Match m in mc)
+            {
+                Console.WriteLine(m);
+            }
+        }
+        static void Main(string[] args)
+        {
+            string str = "A Thousand Splendid Suns";
+
+            Console.WriteLine("Matching words with S ");
+            showMatch(str, @"\bS\S*");
+        }
+    }
+}
