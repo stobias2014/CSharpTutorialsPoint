c7aade98ef22f672b515ee1a97daa9865ae77f49
Saul Tobias <saul.tobias@fdmgroup.com>
2020-04-01 16:39:13 -0500

    added stream reader example

---
+++ StreamReaderApp/StreamReaderApp/Program.cs
@@ -0,0 +1,35 @@
+﻿//reads data from text files
+//
+
+using System;
+using System.IO;
+
+namespace StreamReaderApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try {
+                //create instance of streamreader
+                //using statement closes streamreader
+                using (StreamReader sr = new StreamReader("/Users/saultobias/Desktop/jamaica.rtf"))
+                {
+                    //to store line that is being read
+                    string line;
+
+                    //read and display file lines
+                    while((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch(Exception e) {
+                Console.WriteLine("File could not be read");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
